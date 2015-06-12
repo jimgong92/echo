e2db4d6fca5855d2d4174657d35852b7e0871c5c
Jimmy Gong <gong.jim@gmail.com>
2015-06-11 17:51:36 -0700

    Add getLocation util

---
+++ src/js/utils.js
@@ -0,0 +1,28 @@
+var MAX_CACHE_AGE = 5 * 60 * 1000;
+var MAX_WAIT_TIME = 5 *  1000;
+
+var geoOptions = {
+  timeout: MAX_WAIT_TIME,
+  maximumAge: MAX_CACHE_AGE
+};
+
+var geoError = function(err){
+  console.error("Error occurred:", err);
+}
+
+var getLocation = function(callback){
+  var gps = navigator.geolocation;
+  if(gps){
+    callback(gps.getCurrentPosition(function(position){
+      callback({
+        lat: position.latitude,
+        lon: position.longitude
+      });
+    }, geoError, geoOptions));
+  }
+  else return false;
+}
+
+module.exports = {
+  getLocation: getLocation
+};
